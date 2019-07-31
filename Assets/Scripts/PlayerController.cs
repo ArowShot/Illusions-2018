0b9c451123419b69962c5616ee1d7cc9247eb6ff
Kyle <arow41@gmail.com>
2019-07-31 13:30:06 -0400

    Add assets and old scenes/scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController _controller;
+    private Camera _camera;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _camera = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/StairTeleportScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairTeleportScript : MonoBehaviour
+{
+    public bool goingUpTeleport;//if true, this will be the teleporter taking you from the top to the bottom
+    public Vector3 goingUpTeleportTarget;
+    public Vector3 goingDownTeleportTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        if (goingUpTeleport)
+        {
+            col.gameObject.transform.position = goingUpTeleportTarget;
+        }else if (!goingUpTeleport)
+        {
+            col.gameObject.transform.position = goingDownTeleportTarget;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
