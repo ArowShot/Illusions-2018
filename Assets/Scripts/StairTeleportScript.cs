d772b141807cd74af8a00dbbe45b76a218a1cf30
Joseph Laflamme <joslaflamme3@gmail.com>
2019-08-06 15:59:33 -0400

    Made stair teleport script better, still doesn't work for some reason

--- Assets/Scripts/StairTeleportScript.cs
+++ Assets/Scripts/StairTeleportScript.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 
 public class StairTeleportScript : MonoBehaviour
 {
-    public bool goingUpTeleport;//if true, this will be the teleporter taking you from the top to the bottom
-    public Vector3 goingUpTeleportTarget;
-    public Vector3 goingDownTeleportTarget;
+    
+    public float targetY;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    private void OnControllerColliderHit(ControllerColliderHit col)
+    private void OnTriggerEnter(Collider col)
     {
-        if (goingUpTeleport)
-        {
-            col.gameObject.transform.position = goingUpTeleportTarget;
-        }else if (!goingUpTeleport)
-        {
-            col.gameObject.transform.position = goingDownTeleportTarget;
-        }
+        //if (goingUpTeleport)
+        //{
+        //    Vector3 temp = col.gameObject.transform.position;
+        //    temp.y = goingUpTeleportTarget.y;
+        //    col.gameObject.transform.position = temp;
+        //    Debug.Log("testing top");
+        //}
+        //else if (!goingUpTeleport)
+        //{
+        //    Vector3 temp = col.gameObject.transform.position;
+        //    temp.y = goingDownTeleportTarget.y;
+        //    col.gameObject.transform.position = temp;
+        //    Debug.Log("testing bottom");
+        //}
+        Vector3 temp = col.gameObject.transform.position;
+        temp.y = targetY;
+        col.gameObject.transform.position = temp;
+        Debug.Log("teleporting...");
     }
     // Update is called once per frame
     void Update()
