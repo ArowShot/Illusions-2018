5a0ed945cd697a954ca2f7a40d50164903b0e93c
Joseph Laflamme <joslaflamme3@gmail.com>
2019-08-07 14:25:53 -0400

    Edited stair script

--- Assets/Scripts/StairTeleportScript.cs
+++ Assets/Scripts/StairTeleportScript.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class StairTeleportScript : MonoBehaviour
 {
-    
+    public Transform dest;
     public float targetY;
+    bool wall = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,14 +28,19 @@ public class StairTeleportScript : MonoBehaviour
         //    col.gameObject.transform.position = temp;
         //    Debug.Log("testing bottom");
         //}
-        Vector3 temp = col.gameObject.transform.position;
-        temp.y = targetY;
-        col.gameObject.transform.position = temp;
-        Debug.Log("teleporting...");
+        if (col.gameObject.tag == "Player" && wall) {
+            GameObject player = col.gameObject;
+            //Vector3 temp = new Vector3(col.gameObject.transform.position.x, targetY, col.gameObject.transform.position.z);
+            //temp.y = targetY;
+            //col.gameObject.transform.position = temp;
+            col.gameObject.transform.position = new Vector3(dest.position.x, player.transform.position.y, player.transform.position.z);
+            Debug.Log("teleporting..." + this.gameObject.name);
+            wall = false;
+        }
     }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        wall = true;
     }
 }
